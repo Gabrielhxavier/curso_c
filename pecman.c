fe74aa21abfc7554f89dcd649206621f239c8c95
Gabriel Xavier <gabriel.xavier@ufpe.br>
2025-10-14 16:35:53 -0300

    refatorando o código em funções

--- pecman.c
+++ pecman.c
@@ -1,44 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pecman.h"
 
 char** mapa;                           // declarando uma matriz como ponteiro de ponteiro para alocar sua memoria dinamicamente
 int linhas;
 int colunas;
 
-int main() {
+void liberamapa(){
+    for (int i = 0; i < linhas; i++)
+    {
+        free(mapa[i]);
+    }
+    free(mapa); 
+}
 
+void lemapa(){
     FILE* f;                                    //  declarando o tipo da variável como file (ponteiro) para abrir o arquivo
     
     f = fopen("mapa.txt","r");        
     if (f==0){
         printf("erro.\n");
         exit(1);
-        
     }
     
     fscanf(f,"%d %d", &linhas, &colunas);
 
-    mapa =  malloc(sizeof(char *)*linhas);
-    for (int i=0; i< linhas; i++){
-        mapa[i] = malloc(sizeof(char)*(colunas+1));
-    }
+    alocamapa();
 
     for (int i=0; i < linhas; i++){
         fscanf(f,"%s",mapa[i]);
     }
+    
+    fclose(f);                                  // realizando o fechamento do arquivo
+
+}
+
+void alocamapa(){
+    mapa =  malloc(sizeof(char *)*linhas);
+    for (int i=0; i< linhas; i++){
+        mapa[i] = malloc(sizeof(char)*(colunas+1));
+    }
+}
+
+int main() {
+
+    lemapa();
     
     for (int i=0; i < linhas; i++){
         printf("%s",mapa[i]);
         printf("\n");
     }
 
-    for (int i = 0; i < linhas; i++)
-    {
-        free(mapa[i]);
-    }
-    free(mapa);    
+    liberamapa();   
 
-    fclose(f);                                  // realizando o fechamento do arquivo
-
-    return 0;   
+    return 0;  
 }
