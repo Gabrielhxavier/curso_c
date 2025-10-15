8a9efc70344f7687b56dc1c9fafe1d2e27921cd4
Gabriel Xavier <gabriel.xavier@ufpe.br>
2025-10-15 10:37:29 -0300

    fazendo o herói andar no mapa

--- pecman.c
+++ pecman.c
@@ -42,14 +42,74 @@ void alocamapa(){
     }
 }
 
-int main() {
-
-    lemapa();
-    
+void imprimemapa(){
     for (int i=0; i < linhas; i++){
         printf("%s",mapa[i]);
         printf("\n");
     }
+}
+
+int acabou(){
+    return 0;
+}
+
+void move(char direcao){
+    // localizar o herói no mapa
+    int x, y;                                   // variáveis que vão armazenar a posição atual do herói
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            if (mapa[i][j] == '@')
+            {
+                x = i;
+                y = j;
+            }
+        }  
+    }
+
+    // fazendo o herói andar (w - sobe,a - esquerda,s - desce,d - direita)
+    switch (direcao)
+    {
+    case 'a':
+        mapa[x][y-1] = '@';
+        break;
+       
+    case 'd':
+        mapa[x][y+1] = '@';
+        break;
+
+    case 'w':
+        mapa[x-1][y] = '@';
+        break;
+
+    case 's':
+        mapa[x+1][y] = '@';
+        break;
+    }
+
+    // tirando o herói da antiga posição
+    mapa[x][y] = '.';
+}
+
+int main() {
+
+    lemapa();
+
+    do {
+        imprimemapa();                      // função para imprimir o mapa no loop
+
+        char direcao;                       // char que vai armazenar a mudança de direção do herói
+
+        scanf(" %c", &direcao); 
+        
+        move(direcao);                      // função para mover o herói de acordo com a direção passada
+
+    } 
+    while(!acabou());
+
+    
+    
 
     liberamapa();   
 
